d9cef85d259ce76824b335344056d38058f65dbe
Chad Hansen <chansen.306@gmail.com>
2015-01-31 12:12:39 -0600

    Added enemy and gamestate scripts, changed level.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+
+	public GUIStyle style;
+	public string word;
+	// Use this for initialization
+	void Start () {
+		word = "bakgsdf";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject p = GameObject.FindGameObjectWithTag("Player");
+		Vector2 pos = p.transform.position;
+		
+	}
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI(){
+		TextAboveEnemies();
+	}
+	
+	void TextAboveEnemies(){
+		GameObject[] AllEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+		foreach (GameObject g in AllEnemies){
+			Enemy e = g.GetComponent<Enemy>();
+			
+			Vector3 above = g.transform.position;
+			//needs to be done before converting to screen poitn from world point
+			above.y += 2;
+			Vector3 camPos = Camera.main.WorldToScreenPoint (above);
+			//camPos = new Vector3(Mathf.Clamp (camPos.x, Screen.width),Mathf.Clamp (),camPos.z);
+			
+			GUI.Label(new Rect((camPos.x-20), (Screen.height - camPos.y+10), 100, 50), e.word, e.style);
+		}
+	}
+}
